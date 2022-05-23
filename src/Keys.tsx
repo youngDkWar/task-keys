4fdb078a135a98207fcd62e3e3b9642a2f7cd05a
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-05-23 14:31:31 +0500

    Completed

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,47 @@
+import React, { useState, useRef } from 'react';
+import { idText } from 'typescript';
 import { IItem } from './index';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    let items = props.initialData;
+    if (props.sorting === 'ASC') {
+        items = items.sort((a, b) => a.id - b.id);
+    } else {
+        items = items.sort((a, b) => b.id - a.id);
+    }
+    return (
+        <ol>
+            {items.map((item) => (
+                <KeyItem key={item.id} item={item} />
+            ))}
+        </ol>
+    );
+}
+
+function KeyItem(props: { item: IItem }) {
+    function keysEvents(pressedKey: React.KeyboardEvent<HTMLInputElement>) {
+        if (pressedKey.key === 'Enter') {
+            setOriginalName(input.current?.value || '');
+            setStatus(false);
+        }
+        if (pressedKey.key === 'Escape') {
+            setStatus(false);
+        }
+    }
+
+    const [originalName, setOriginalName] = useState(props.item.name);
+    const input = useRef<HTMLInputElement>(null);
+    const [chngStatus, setStatus] = useState(false);
+
+    return !chngStatus ? (
+        <li onClick={() => setStatus(true)}>{originalName}</li>
+    ) : (
+        <li key={props.item.id}>
+            <input
+                ref={input}
+                onKeyDown={keysEvents}
+                defaultValue={originalName}
+            />
+        </li>
+    );
 }
